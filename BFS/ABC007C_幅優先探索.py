0d18e91239768db7185078ead451602a89b4ced0
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2020-12-01 22:01:41 +0900

    Update ABC007C_幅優先探索.py

--- BFS/ABC007C_幅優先探索.py
+++ BFS/ABC007C_幅優先探索.py
@@ -8,10 +8,10 @@ Q = deque([[SX-1,SY-1]])
 S[SX-1][SY-1] = 0
 
 while Q:
-  x,y = Q.popleft()
-  for dx,dy in D:
-    if S[x+dx][y+dy]==".":
-      S[x+dx][y+dy] = S[x][y]+1
-      Q.append([x+dx,y+dy])
+	x,y = Q.popleft()
+	for dx,dy in D:
+		if S[x+dx][y+dy]==".":
+			S[x+dx][y+dy] = S[x][y]+1
+			Q.append([x+dx,y+dy])
 
 print(S[GX-1][GY-1])
