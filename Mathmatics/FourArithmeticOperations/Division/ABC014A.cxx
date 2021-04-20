54444752cddfebd9771e67c64b3a2d175ac19f6b
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-04-20 20:07:45 +0900

    added ABC014A

---
+++ Mathmatics/FourArithmeticOperations/Division/ABC014A.cxx
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+
+    if(a%b==0)
+    {
+        cout<<0<<endl;
+    }
+
+    else
+    {
+        cout<<b-a%b<<endl;
+    }
+}
