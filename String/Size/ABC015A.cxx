e22a59e29e0112915055c18a92b6b499725ba357
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-04-20 20:12:59 +0900

    added ABC015A

---
+++ String/Size/ABC015A.cxx
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string A;
+    string B;
+    cin>>A;
+    cin>>B;
+
+    if(A.size()<B.size())
+    {
+        cout<<B<<endl;
+    }
+
+    else
+    {
+        cout<<A<<endl;
+    }
+}
