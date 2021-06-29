868852eb57e3a87c39aba79e7c3e7908868e8730
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-06-29 22:08:09 +0900

    added ABC020A

---
+++ ConditionalBranch/ABC020A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int Q;
+    std::cin>>Q;
+
+    if(Q==1)
+    {
+        std::cout<<"ABC"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"chokudai"<<std::endl;
+    }
+}
