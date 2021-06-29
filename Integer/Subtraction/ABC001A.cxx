59e1199b89bca839eac29b9cb3665d3cfa942399
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-06-29 20:51:55 +0900

    updated ABC001A

--- Integer/Subtraction/ABC001A.cxx
+++ Integer/Subtraction/ABC001A.cxx
@@ -1,11 +1,8 @@
 #include<bits/stdc++.h>
-using namespace std;
 
 int main()
 {
-    int h1;
-    int h2;
-    cin>>h1;
-    cin>>h2;
-    cout<<h1-h2<<endl;
+    int h1,h2;
+    std::cin>>h1,h2;
+    std::cout<<h1-h2<<std::endl;
 }
