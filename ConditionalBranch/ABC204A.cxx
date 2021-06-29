f3ee4411444fc1db7009c4bc4737eea172ed2da0
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-06-29 20:19:08 +0900

    added ABC204A

---
+++ ConditionalBranch/ABC204A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int x,y;
+    std::cin>>x>>y;
+
+    if(x==y)
+    {
+        std::cout<<x<<std::endl;
+    }
+    else
+    {
+        std::cout<<3-x-y<<std::endl;
+    }
+}
