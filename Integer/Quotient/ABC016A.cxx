f7363691f3a7e3427ddbd902c4e5c408e29ba5a1
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-06-29 21:35:04 +0900

    added ABC016A

---
+++ Integer/Quotient/ABC016A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int M,D;
+    std::cin>>M>>D;
+
+    if(M%D==0)
+    {
+        std::cout<<"YES"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"NO"<<std::endl;
+    }
+}
