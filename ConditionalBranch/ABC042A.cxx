df213666ef6082abb2b1de566a24530c2b7e15dc
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-01 19:13:00 +0900

    added ABC042A

---
+++ ConditionalBranch/ABC042A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B,C;
+    std::cin>>A>>B>>C;
+
+    if(A+B+C==17)
+    {
+        std::cout<<"YES"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"NO"<<std::endl;
+    }
+}
