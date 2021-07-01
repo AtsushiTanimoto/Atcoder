622bb9242375037e263b7721660554f28faeee88
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-01 17:35:32 +0900

    added ABC034A

---
+++ ConditionalBranch/ABC034A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int x,y;
+    std::cin>>x>>y;
+
+    if(x<y)
+    {
+        std::cout<<"Better"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Worse"<<std::endl;
+    }
+}
