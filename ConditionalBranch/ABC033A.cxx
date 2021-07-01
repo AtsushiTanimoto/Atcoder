104dbe63fe202c9ba02e399e6bd275ffa9cee192
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-01 17:30:47 +0900

    added ABC033A

---
+++ ConditionalBranch/ABC033A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int N;
+    std::cin>>N;
+
+    if(N%1111==0)
+    {
+        std::cout<<"SAME"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"DIFFERENT"<<std::endl;
+    }
+}
