851528de80b708356f8cf2d039094bed47b12534
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-01 20:34:32 +0900

    added ABC049A

---
+++ ConditionalBranch/ABC049A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string c;
+    std::cin>>c;
+    
+    if(c=="a"||c=="e"||c=="i"||c=="o"||c=="u")
+    {
+        std::cout<<"vowel"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"consonant"<<std::endl;
+    }
+}
