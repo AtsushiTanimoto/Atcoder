769bc93e650281bcaf75129df484bf8467e70f58
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-01 17:20:54 +0900

    added ABC032A

---
+++ Search/LinearSearch/ABC032A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int a,b,n;
+    std::cin>>a>>b>>n;
+
+    for(int i=n;i<=100000;i++)
+    {
+        if(i%a==0 && i%b==0)
+        {
+            std::cout<<i<<std::endl;
+            break;
+        }
+    }
+}
