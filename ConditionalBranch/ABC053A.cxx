35438f63c95615df616b669ae014faf6b787eea7
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-02 14:45:12 +0900

    added ABC053A

---
+++ ConditionalBranch/ABC053A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int x;
+    std::cin>>x;
+
+    if(x<1200)
+    {
+        std::cout<<"ABC"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"ARC"<<std::endl;
+    }
+}
