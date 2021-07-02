1321e722cb54ecd1c672acb14f4c697ecd874bea
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-02 18:23:08 +0900

    added ABC070A

---
+++ Branch/SingleBranch/ABC070A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string N;
+    std::cin>>N;
+
+    if(N[0]==N[2])
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
