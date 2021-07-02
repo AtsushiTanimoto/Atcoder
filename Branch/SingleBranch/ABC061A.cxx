f517faab55df8732881a78658214faf5cfce5954
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-02 17:39:45 +0900

    added ABC061A

---
+++ Branch/SingleBranch/ABC061A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B,C;
+    std::cin>>A>>B>>C;
+
+    if(A<=C && C<=B)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
