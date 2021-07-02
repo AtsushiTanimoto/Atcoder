fb3af875d89af078333abc0852ab161a472a8820
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-02 14:56:10 +0900

    added ABC056A

---
+++ ConditionalBranch/ABC056A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string A,B;
+    std::cin>>A>>B;
+
+    if(A==B)
+    {
+        std::cout<<"H"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"D"<<std::endl;
+    }
+}
