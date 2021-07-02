4241ef1b5e40ecbd35d85d8efb81af5a4063268a
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-02 17:59:26 +0900

    added ABC065A

---
+++ Branch/MultipleBranch/ABC065A.cxx
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int X,A,B;
+    std::cin>>X>>A>>B;
+
+    if(B<=A)
+    {
+        std::cout<<"delicious"<<std::endl;
+    }
+    else if(B<=A+X)
+    {
+        std::cout<<"safe"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"dangerous"<<std::endl;
+    }
+}
