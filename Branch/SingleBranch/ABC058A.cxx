8e52f0dbbf8fe823ee07b32ce73dbe96765e5ee9
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-02 15:33:55 +0900

    added ABC058A

---
+++ Branch/SingleBranch/ABC058A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int a,b,c;
+    std::cin>>a>>b>>c;
+
+    if(b-a==c-b)
+    {
+        std::cout<<"YES"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"NO"<<std::endl;
+    }
+}
