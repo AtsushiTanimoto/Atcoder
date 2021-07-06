3bc22444f2a9c639f1ba7df6561a1039b9a182c6
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 17:28:25 +0900

    added ABC086A

---
+++ Branch/SingleBranch/ABC086A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B;
+    std::cin>>A>>B;
+
+    if(A*B%2==0)
+    {
+        std::cout<<"Even"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Odd"<<std::endl;
+    }
+}
