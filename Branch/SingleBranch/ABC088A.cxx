d82eb49c8c6e12acf9053de80298d0ebabe4680b
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 17:35:41 +0900

    added ABC088A

---
+++ Branch/SingleBranch/ABC088A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int N,A;
+    std::cin>>N>>A;
+
+    if(N%500<=A)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
