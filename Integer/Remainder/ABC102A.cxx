19d2bc620f207c97f5ec2eb95ea344c3fb5ef3df
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 20:37:26 +0900

    added ABC102A

---
+++ Integer/Remainder/ABC102A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int N;
+    std::cin>>N;
+
+    if(N%2==0)
+    {
+        std::cout<<N<<std::endl;
+    }
+    else
+    {
+        std::cout<<2*N<<std::endl;
+    }
+}
