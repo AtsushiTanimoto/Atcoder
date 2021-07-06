a75d9baa3e5dfa22c546b434a842f45ece6864d7
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 18:43:50 +0900

    added ABC096A

---
+++ Branch/SingleBranch/ABC096A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B;
+    std::cin>>A>>B;
+
+    if(A<=B)
+    {
+        std::cout<<A<<std::endl;
+    }
+    else
+    {
+        std::cout<<A-1<<std::endl;
+    }
+}
