147de04e69426362284a32bedeac628af872c093
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 20:02:06 +0900

    added ABC099A

---
+++ Integer/Comparison/ABC099A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int N;
+    std::cin>>N;
+
+    if(N<=999)
+    {
+        std::cout<<"ABC"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"ABD"<<std::endl;
+    }
+}
