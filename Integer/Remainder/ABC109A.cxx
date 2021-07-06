fbaa48e6e1c9e5b115686f57d5a2e53f2b16fb1e
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 21:04:44 +0900

    added ABC109A

---
+++ Integer/Remainder/ABC109A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B;
+    std::cin>>A>>B;
+
+    if(A*B%2!=0)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
