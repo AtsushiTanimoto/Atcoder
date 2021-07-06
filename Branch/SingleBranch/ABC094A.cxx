9255c9212bd547ec28e8a185e81a8ca90c0ff550
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-06 18:28:04 +0900

    added ABC095A

---
+++ Branch/SingleBranch/ABC094A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B,X;
+    std::cin>>A>>B>>X;
+
+    if(A<=X&&X<=A+B)
+    {
+        std::cout<<"YES"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"NO"<<std::endl;
+    }
+}
