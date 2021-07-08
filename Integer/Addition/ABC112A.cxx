8d91168dae61771d344ddea221e227787a69f56c
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-08 20:40:02 +0900

    added ABC112A

---
+++ Integer/Addition/ABC112A.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int N,A,B;
+    std::cin>>N;
+
+    if(N==1)
+    {
+        std::cout<<"Hello World"<<std::endl;
+    }
+    else
+    {
+        std::cin>>A>>B;
+        std::cout<<A+B<<std::endl;
+    }
+}
