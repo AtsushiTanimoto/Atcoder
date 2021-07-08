df5da9de7e77818db7a6f488cb3ce5d1e0ee4913
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-08 20:47:45 +0900

    added ABC115A

---
+++ Integer/Comparison/ABC115A.cxx
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int D;
+    std::cin>>D;
+
+    if(D==22)
+    {
+        std::cout<<"Christmas Eve Eve Eve"<<std::endl;
+    }
+    else if(D==23)
+    {
+        std::cout<<"Christmas Eve Eve"<<std::endl;
+    }
+    else if(D==24)
+    {
+        std::cout<<"Christmas Eve"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Christmas"<<std::endl;
+    }
+}
