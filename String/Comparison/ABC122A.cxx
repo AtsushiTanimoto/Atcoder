cc9410856c0cb545f493107419228be7120bf2c5
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-08 21:23:32 +0900

    added ABC122A

---
+++ String/Comparison/ABC122A.cxx
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string S;
+    std::cin>>S;
+
+    if(S=="A")
+    {
+        std::cout<<"T"<<std::endl;
+    }
+    else if(S=="T")
+    {
+        std::cout<<"A"<<std::endl;
+    }
+    else if(S=="G")
+    {
+        std::cout<<"C"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"G"<<std::endl;
+    }
+}
