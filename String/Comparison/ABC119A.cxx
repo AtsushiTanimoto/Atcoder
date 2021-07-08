3df7da94691036fbd512d7a9fd2c18d12b6c673a
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-08 21:09:58 +0900

    added ABC119A

---
+++ String/Comparison/ABC119A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string S;
+    std::cin>>S;
+
+    if(S<="2019/04/30")
+    {
+        std::cout<<"Heisei"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"TBD"<<std::endl;
+    }
+}
