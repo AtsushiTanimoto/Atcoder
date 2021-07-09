5f45195a56c019d83bfc8ed80bde30f3257bff63
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-09 15:41:07 +0900

    added ABC138A

---
+++ String/Comparison/ABC138A.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A;
+    std::string S;
+    std::cin>>A>>S;
+
+    if(3200<=A)
+    {
+        std::cout<<S<<std::endl;
+    }
+    else
+    {
+        std::cout<<"red"<<std::endl;
+    }
+}
