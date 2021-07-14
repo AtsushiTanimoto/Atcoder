868bda846bc675515f783607ffed50d7b7006b77
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-14 23:19:53 +0900

    added ABC150A

---
+++ Integer/Multiplication/ABC150A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int K,X;
+    std::cin>>K>>X;
+
+    if(X<=500*K)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
