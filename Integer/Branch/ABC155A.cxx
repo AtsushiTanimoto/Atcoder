e81c935d23953cd1050c72e5066807d84a104a9c
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-26 18:47:32 +0900

    added ABC155A

---
+++ Integer/Branch/ABC155A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int A,B,C;
+    std::cin>>A>>B>>C;
+
+    if((A==B&&B!=C)||(B==C&&C!=A)||(C==A&&A!=B))
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
