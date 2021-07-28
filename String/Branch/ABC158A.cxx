a33f52109461f92f4ad5f9f5179211ec363017f5
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-07-28 17:38:20 +0900

    added ABC158A

---
+++ String/Branch/ABC158A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string S;
+    std::cin>>S;
+
+    if(S[0]!=S[1]||S[1]!=S[2])
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
