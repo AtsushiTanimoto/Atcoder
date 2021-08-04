ef362d4ed422380778f07e90d0e52e061377b7da
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-04 20:21:53 +0900

    added ABC166A

---
+++ String/Branch/ABC166A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string S;
+    std::cin>>S;
+
+    if(S=="ABC")
+    {
+        std::cout<<"ARC"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"ABC"<<std::endl;
+    }
+}
