497b9425d5183aa67f0b4e88f3cb97ba18596d2b
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-04 20:03:07 +0900

    added ABC162A

---
+++ String/Branch/ABC162A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::string S;
+    std::cin>>S;
+
+    if(S[0]=='7'||S[1]=='7'||S[2]=='7')
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
