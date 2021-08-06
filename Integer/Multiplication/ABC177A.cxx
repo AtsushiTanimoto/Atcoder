52f8981a29f620a4bed935d3d3e3b228dca68d09
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-06 18:08:48 +0900

    added ABC177A

---
+++ Integer/Multiplication/ABC177A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int D,T,S;
+    std::cin>>D>>T>>S;
+
+    if(D<=S*T)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
