3025d89e121e411debfaf5cc36278b1d88723276
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-06 17:38:44 +0900

    added ABC171A

---
+++ String/Branch/ABC171A.cxx
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    char C;
+    std::cin>>C;
+
+    if(std::isupper(C))
+    {
+        std::cout<<"A"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"a"<<std::endl;
+    }
+}
