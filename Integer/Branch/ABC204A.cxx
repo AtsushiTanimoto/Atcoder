34abf5f574f0192921eb96bfa8d00d13ab17948a
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-25 16:31:22 +0900

    Added ABC204A

---
+++ Integer/Branch/ABC204A.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    int X,Y;
+    std::cin>>X>>Y;
+
+    if(X==Y)
+    {
+        std::cout<<X<<std::endl;
+    }
+    else
+    {
+        std::cout<<3-X-Y<<std::endl;
+    }
+}
