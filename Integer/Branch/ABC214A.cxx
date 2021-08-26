01823a8986cd0bdb12f0e0a92d3a56d0c22b713a
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-26 16:11:24 +0900

    Added ABC214A

---
+++ Integer/Branch/ABC214A.cxx
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    int N;
+    std::cin>>N;
+
+    if(N<=125)
+    {
+        std::cout<<4<<std::endl;
+    }
+    else if(N<=211)
+    {
+        std::cout<<6<<std::endl;
+    }
+    else
+    {
+        std::cout<<8<<std::endl;
+    }
+}
