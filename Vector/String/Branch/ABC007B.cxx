e325997ced341b25ef4e9e34c09f70b728b44038
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-26 18:03:03 +0900

    Added ABC007B

---
+++ Vector/String/Branch/ABC007B.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    std::string A;
+    std::cin>>A;
+
+    if(A=="a")
+    {
+        std::cout<<-1<<std::endl;
+    }
+    else
+    {
+        std::cout<<'a'<<std::endl;
+    }
+}
