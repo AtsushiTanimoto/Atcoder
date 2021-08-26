7ed4ac544865d2b63da9bf50856b76c4368d6783
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-26 16:45:25 +0900

    Updated ABC022A

--- Search/LinearSearch/ABC022A.cxx
+++ Search/LinearSearch/ABC022A.cxx
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 
+
 int main()
 {
+    int ans = 0;
     int N,S,T,W,A;
-    int C = 0;
     std::cin>>N>>S>>T>>W;
 
     if(S<=W&&W<=T)
     {
-        C+=1;
+        ans+=1;
     }
 
     for(int n=1;n<N;n++)
@@ -18,9 +19,9 @@ int main()
 
         if(S<=W&&W<=T)
         {
-            C+=1;
+            ans+=1;
         }
     }
 
-    std::cout<<C<<std::endl;
+    std::cout<<ans<<std::endl;
 }
