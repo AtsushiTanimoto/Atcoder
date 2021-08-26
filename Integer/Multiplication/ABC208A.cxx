a0d8ac4eefc6ec7060081ec1032d9015c4940b34
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-26 15:44:46 +0900

    Added ABC208A

---
+++ Integer/Multiplication/ABC208A.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    int A,B;
+    std::cin>>A>>B;
+
+    if(A<=B&&B<=6*A)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
