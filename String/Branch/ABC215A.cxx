209da1c4c2639ecc31f94d310fbf39ed1f0d032f
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-08-26 16:14:33 +0900

    Added ABC215A

---
+++ String/Branch/ABC215A.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    std::string S;
+    std::cin>>S;
+
+    if(S=="Hello,World!")
+    {
+        std::cout<<"AC"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"WA"<<std::endl;
+    }
+}
