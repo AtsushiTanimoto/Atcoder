aff7ce0c3653264ea65c6247d2917d16209732db
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-06-20 15:51:51 +0900

    Changes to be committed: 	new file:   Matrix/Extraction/ABC255A.cxx 	new file:   Scalar/Integer/Exponetiation/ABC256A.cxx

---
+++ Matrix/Extraction/ABC255A.cxx
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    int R,C,A[3][3];
+    std::cin>>R>>C>>A[1][1]>>A[1][2]>>A[2][1]>>A[2][2];
+    std::cout<<A[R][C]<<std::endl;
+}
---
+++ Scalar/Integer/Exponetiation/ABC256A.cxx
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    int N;
+    std::cin>>N;
+    int ans = std::pow(2,N);
+    std::cout<<ans<<std::endl;
+}
