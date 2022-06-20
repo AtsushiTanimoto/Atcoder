a6269d659978724b69adaed9761f18cff5fa3f3c
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-06-20 15:58:10 +0900

    Changes to be committed: 	new file:   String/Branch/ABC217A.cxx 	new file:   String/Branch/ABC218A.cxx

---
+++ String/Branch/ABC217A.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    std::string S,T;
+    std::cin>>S>>T;
+
+    if(S<T)
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
---
+++ String/Branch/ABC218A.cxx
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+
+int main()
+{
+    int N;
+    std::string S;
+    std::cin>>N>>S;
+
+    if(S[N-1]=='o')
+    {
+        std::cout<<"Yes"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"No"<<std::endl;
+    }
+}
